e50b34b4ab6550e7a4ce80108b335f62ad7cd85c
NicolaAssolini98 <64692346+NicolaAssolini98@users.noreply.github.com>
2024-04-17 11:34:56 +0200

    cfg_build.py: corrected branching error

--- cfg_build.py
+++ cfg_build.py
@@ -5,7 +5,7 @@ import networkx as nx
 from matplotlib import pyplot as plt
 
 pattern_gate_app = r'^\s*([\w\s,]+)\s*=\s*([\w\s,]+\([\w\s,]+\))\s*$'
-pattern_init = r'^(\s*\w+\s*(\s*,\s*\w)*)\s*=\s*qubit\(\s*\)(\s*,\s*qubit\(\s*\))*'
+pattern_init = r'^(\s*\w+\s*(\s*,\s*\w+)*)\s*=\s*qubit\(\s*\)(\s*,\s*qubit\(\s*\))*'
 pattern_measure = r'\w'
 node_count = 1
 exit_node = 'Exit'
@@ -44,17 +44,6 @@ def print_cfg(cfg):
     nx.draw_networkx_edge_labels(cfg, pos, edge_labels=labels, font_size=7)
     plt.title("Control Flow Graph with Edge Labels")
     plt.show()
-
-
-def clean_empty_line(lines):
-    res = []
-    for line in lines:
-        for char in line:
-            if not char.isspace():
-                res.append(line)
-                break
-
-    return res
 
 
 def clean_var_names(var_names):
@@ -107,21 +96,28 @@ def extract_function_args(string):
         return []
 
 
-def extract_sub_graph(cfg, lines, if_branch_size, if_node, end_node, tabs):
-    if_branch_lines = []
-    while count_tab(lines[if_branch_size]) > tabs or if_branch_size >= len(lines):
-        if_branch_lines.append(lines[if_branch_size])
-        if_branch_size += 1
-    if_cfg = nx.DiGraph()
-    if_cfg.add_node(if_node)
-    if_cfg = build_cfg(if_branch_lines, if_cfg, if_node)
-    if_end_nodes = [node for node in if_cfg.nodes if if_cfg.out_degree(node) == 0]
-    cfg = nx.compose(cfg, if_cfg)
-    for if_end_node in if_end_nodes:
-        if if_end_node != exit_node:
-            cfg = nx.relabel_nodes(cfg, {if_end_node: end_node})
+def extract_sub_graph(cfg, lines, head_node, end_node, tabs):  #, if_branch_size, , tabs):
+    branch_lines = []
+    end_index = 0
+    for line in lines:
+        # print(line)
+        if count_tab(line) > tabs:
+            branch_lines.append(line)
+            end_index += 1
+        else:
+            break
 
-    return cfg, if_branch_size
+    branch_cfg = nx.DiGraph()
+    branch_cfg.add_node(head_node)
+    branch_cfg = build_cfg(branch_lines, branch_cfg, head_node)
+
+    branch_end_nodes = [node for node in branch_cfg.nodes if branch_cfg.out_degree(node) == 0]
+    cfg = nx.compose(cfg, branch_cfg)
+    for branch_end_node in branch_end_nodes:
+        if branch_end_node != exit_node:
+            cfg = nx.relabel_nodes(cfg, {branch_end_node: end_node})
+
+    return cfg, lines[end_index:]
 
 
 def build_cfg(lines, cfg=None, prev_node=None):
@@ -129,17 +125,7 @@ def build_cfg(lines, cfg=None, prev_node=None):
     if len(lines) == 0:
         return cfg
     l = lines[0]
-
-    if lines[0].replace(" ", "").startswith('#'):
-        return build_cfg(lines[1:], cfg, prev_node)
-
-    if 'pass' in lines[0]:
-        p_node = new_node()
-        cfg.add_node(p_node)
-        cfg.add_edge(prev_node, p_node, label=EdgeLabel(NodeType.Skip, ''))
-
-        return build_cfg(lines[1:], cfg, p_node)
-
+    print(l)
     if 'def' in lines[0]:
         # def fun(x: qubit, v: qubit , _, )...:
         graph = nx.DiGraph()
@@ -150,8 +136,16 @@ def build_cfg(lines, cfg=None, prev_node=None):
         # ['x','y',]
         p_node = new_node()
         graph.add_edge(start_node, p_node, label=EdgeLabel(NodeType.Args, args))
+        fun_name = lines[0].replace(" ", "").split('(')[0][3:]
 
-        return build_cfg(lines[1:], graph, p_node)
+        return fun_name, build_cfg(lines[1:], graph, p_node)
+
+    if 'pass' in lines[0]:
+        p_node = new_node()
+        cfg.add_node(p_node)
+        cfg.add_edge(prev_node, p_node, label=EdgeLabel(NodeType.Skip, ''))
+
+        return build_cfg(lines[1:], cfg, p_node)
 
     if 'return' in lines[0]:
         # return x,y,_,...
@@ -164,60 +158,60 @@ def build_cfg(lines, cfg=None, prev_node=None):
         # discard(x,y,...)
         p_node = new_node()
         cfg.add_node(p_node)
-        cfg.add_edge(prev_node, p_node, label=EdgeLabel(NodeType.Discard, lines[0].replace(" ", "").replace("\n", "")[8:-1].split(',')))
+        cfg.add_edge(prev_node, p_node,
+                     label=EdgeLabel(NodeType.Discard, lines[0].replace(" ", "").replace("\n", "")[8:-1].split(',')))
         return build_cfg(lines[1:], cfg, p_node)
 
     if 'measure' in lines[0]:
         # _ = measure(x,y,...)
         p_node = new_node()
         cfg.add_node(p_node)
-        part = lines[0].replace(" ", "").replace("\n","").split('=')
+        part = lines[0].replace(" ", "").replace("\n", "").split('=')
         m_vars = clean_var_names(part[1][8:-1].split(','))
         cfg.add_edge(prev_node, p_node, label=EdgeLabel(NodeType.Measure, m_vars))
         return build_cfg(lines[1:], cfg, p_node)
 
     if 'if' in lines[0]:
         tabs = count_tab(lines[0])
-
+        cond = lines[0].replace(" ", "").replace("\n", "")[2:-1]
         if_node = new_node()
         cfg.add_node(if_node)
-        cfg.add_edge(prev_node, if_node, label=EdgeLabel(NodeType.NonZero, lines[0].replace(" ", "").replace("\n", "")[2:-1]))
+        cfg.add_edge(prev_node, if_node,
+                     label=EdgeLabel(NodeType.NonZero, cond))
         end_node = new_node()
         cfg.add_node(end_node)
+        cfg, lines_after_if = extract_sub_graph(cfg, lines[1:], if_node, end_node, tabs)
 
-        cfg, if_branch_size = extract_sub_graph(cfg, lines, 1, if_node, end_node, tabs)
-
-        else_branch_size = if_branch_size + 1
-        if 'else' in lines[if_branch_size]:
+        if len(lines_after_if) > 0 and 'else' in lines_after_if[0]:
             else_node = new_node()
             cfg.add_node(else_node)
-            cfg.add_edge(prev_node, else_node, label=EdgeLabel(NodeType.Zero, lines[0].replace(" ", "").replace("\n", "")[2:-1]))
-
-            cfg, else_branch_size = extract_sub_graph(cfg, lines, else_branch_size, else_node, end_node, tabs)
+            cfg.add_edge(prev_node, else_node, label=EdgeLabel(NodeType.Zero, cond))
+            cfg, lines_after_if = extract_sub_graph(cfg, lines_after_if[1:], else_node, end_node, tabs)
         else:
-            cfg.add_edge(prev_node, end_node, label=EdgeLabel(NodeType.Zero, lines[0].replace(" ", "").replace("\n", "")[2:-1]))
+            cfg.add_edge(prev_node, end_node, label=EdgeLabel(NodeType.Zero, cond))
 
-        return build_cfg(lines[else_branch_size:], cfg, end_node)
+        return build_cfg(lines_after_if, cfg, end_node)
 
-    if 'while' in lines[0]:
-        # if end_node is None:
-        #     end_node = new_node()
-        #     cfg.add_node(end_node)
+    if 'while' in lines[0] or 'for' in lines[0]:
         tabs = count_tab(lines[0])
-
+        if 'while' in lines[0]:
+            # while cond:
+            cond = lines[0].replace(" ", "").replace("\n", "")[5:-1]
+        else:
+            # for v in cond:
+            cond = lines[0].replace(" ", "").replace("\n", "")[3:-1]
+            cond = cond.replace('in', ' ')
         while_node = new_node()
         cfg.add_node(while_node)
-        cfg.add_edge(prev_node, while_node, label=EdgeLabel(NodeType.NonZero, lines[0].replace(" ", "").replace("\n", "")[5:-1]))
+        cfg.add_edge(prev_node, while_node, label=EdgeLabel(NodeType.NonZero, cond))
 
-        cfg, while_branch_size = extract_sub_graph(cfg, lines, 1, while_node, prev_node, tabs)
+        cfg, lines_after_while = extract_sub_graph(cfg, lines[1:], while_node, prev_node, tabs)
 
         end_node = new_node()
         cfg.add_node(end_node)
-        cfg.add_edge(prev_node, end_node, label=EdgeLabel(NodeType.Zero, lines[0].replace(" ", "").replace("\n", "")[5:-1]))
+        cfg.add_edge(prev_node, end_node, label=EdgeLabel(NodeType.Zero, cond))
 
-        return build_cfg(lines[while_branch_size:], cfg, end_node)
-
-        # TODO for loop
+        return build_cfg(lines_after_while, cfg, end_node)
 
     if re.match(pattern_gate_app, lines[0]):
         # x,y = gate(x,y)
@@ -243,4 +237,5 @@ def build_cfg(lines, cfg=None, prev_node=None):
 
         return build_cfg(lines[1:], cfg, p_node)
 
-    exit('error parsing: ' + str(lines))
+    exit('error parsing: ' + l)
+
