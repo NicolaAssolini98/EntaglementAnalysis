48c4c2c00da01eef3bb1ff712dae2caba97c3ec8
NicolaAssolini98 <nicola.assolini.98@gmail.com>
2024-08-03 17:39:07 +0200

    start defining ent analysis

--- cfg_build.py
+++ cfg_build.py
@@ -2,6 +2,7 @@ import re
 from enum import Enum
 
 import networkx as nx
+from lark import Tree, Token
 from matplotlib import pyplot as plt
 
 pattern_gate_app = r'^\s*([\w\s,]+)\s*=\s*([\w\s,]+\([\w\s,]+\))\s*$'
@@ -10,19 +11,6 @@ pattern_measure = r'\w'
 node_count = 1
 exit_node = 'Exit'
 start_node = 'Start'
-
-
-class EdgeLabel:
-    def __init__(self, type_label, value):
-        self.value = value
-        self.type = type_label
-
-    def __str__(self):
-        return str(self.type).split(".")[1] + ": " + str(self.value)
-
-    def __repr__(self):
-        return self.__str__()
-
 
 """
 P(q)∣
@@ -43,11 +31,78 @@ class NodeType(Enum):
     CX = 6
 
 
-def print_cfg(cfg):
-    pos = nx.spring_layout(cfg)
-    labels = nx.get_edge_attributes(cfg, 'label')
-    nx.draw(cfg, pos, with_labels=True, node_size=300, node_color="lightblue", font_size=8, font_color="black",
-            font_weight="bold", arrows=True)
-    nx.draw_networkx_edge_labels(cfg, pos, edge_labels=labels, font_size=7)
-    plt.title("Control Flow Graph with Edge Labels")
-    plt.show()
+class EdgeLabel:
+    def __init__(self, type_label, value):
+        self.value = value
+        self.type = type_label
+
+    def __str__(self):
+        return str(self.type).split(".")[1] + ": " + str(self.value)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+
+def new_node():
+    global node_count
+    node = str(node_count)
+    node_count = node_count + 1
+    return node
+
+
+def get_variables(tree):
+    """
+    :type tree: Tree
+    :return: list of variables
+    """
+    if isinstance(tree, Tree):
+        if tree.data == 'decl':
+            return [child for child in tree.children]
+
+    exit('Invalid AST')
+
+
+def build_graph(trees, cfg, prev_node):
+    """
+    :return:
+    """
+    if len(trees) == 0:
+        return cfg
+
+    for t in trees:
+        print(t.data)
+
+    if isinstance(trees[0], Tree):
+        if trees[0].data == 'cx_stmt':
+            p_node = new_node()
+            cfg.add_node(p_node)
+            cfg.add_edge(prev_node, p_node, label=EdgeLabel(NodeType.CX, []))
+
+            return build_graph(trees[1:], cfg, p_node)
+        if trees[0].data == 't_stmt':
+            pass
+        if trees[0].data == 'h_stmt':
+            pass
+
+    if isinstance(trees[0], Token):
+        if trees[0].value == 'pass_stmt':
+            p_node = new_node()
+            cfg.add_node(p_node)
+            cfg.add_edge(prev_node, p_node, label=EdgeLabel(NodeType.Skip, ''))
+
+            return build_graph(trees[1:], cfg, p_node)
+        if trees[0].type == 'NAME':
+            return trees[0].value
+
+
+def init_cfg(ast):
+    if isinstance(ast, Tree):
+        if ast.data == 'start':
+            graph = nx.DiGraph()
+            p_vars = get_variables(ast.children[0])
+            graph.add_node(start_node)
+
+            return p_vars, build_graph(ast.children[1:], graph, start_node)
+
+    exit('Invalid AST')
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from analysis2 import *
+from cfg_build import *
 from lark import Lark
 from lark.indenter import PythonIndenter
 
@@ -6,8 +7,20 @@ from lark.indenter import PythonIndenter
 kwargs = dict(postlex=PythonIndenter())
 parser = Lark.open("lark/grammar_l1.lark", rel_to=__file__, parser="lalr", **kwargs)
 
+text = '''
+[a,b,c,d,e]
+# cx(a,b)
+skip
+# if a:
+#     t(b)
+# else:
+#     h(b)
+# while c:
+#     h(c)
+'''
 
 
-# t = parser.parse(text)
-# print(t)
-# print(t.pretty())
+t = parser.parse(text)
+print(t)
+print(t.pretty())
+print(init_cfg(t))
